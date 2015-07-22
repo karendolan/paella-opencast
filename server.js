e4092ce629648d049bdbb42474fa92ed5c1e79c7
Jim Kang <jimkang@gmail.com>
2015-07-22 12:02:25 -0400

    Updated test server to: - Serve over https (with test certificate) to placate requests within our code. - Serve from the local resources we want to test when appropriate. - Forward login.html requests to whatever watch.html* URL the browser originally requested.

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
-var express = require('express')
-  , httpProxy = require('http-proxy');
+var express = require('express');
+var httpProxy = require('http-proxy');
+var https = require('https');
+var fs = require('fs');
+
+var matterhornProxyURL = 'https://matterhorn.dce.harvard.edu/';
+var mostRecentWatchReqUrl;
 
 var app = express();
 
+var proxy = httpProxy.createProxyServer({
+  secure: false
+});
 
-var proxy = httpProxy.createProxyServer({});
- 
-function proxyFunc(req, res, next) {
-	//proxy.web(req, res, { target: 'http://engage.opencast.org/' });
-	proxy.web(req, res, { target: 'http://videoapuntes-engage.upv.es:8080/' });
-}
+// Serve /engage/player/* requests from the local build folder.
+app.use('/engage/player', express.static('build'));
+
+// Handle login.html requests with a redirect.
+app.get('/login.html', handleLogin);
+
+// Handle everything else with the proxy back to the Matterhorn server.
 app.use(proxyFunc);
 
 
-module.exports = app;
+function handleLogin(req, res, next) {
+  if (mostRecentWatchReqUrl) {
+    res.redirect(mostRecentWatchReqUrl);
+  }
+  else {
+    next();
+  }
+}
+
+function proxyFunc(req, res, next) {
+  console.log('Proxying:', req.url);
+
+  if (req.url.indexOf('/player/watch.html') === 0) {
+    mostRecentWatchReqUrl = req.url;
+  }
+
+  proxy.web(
+    req,
+    res,
+    {
+      target: matterhornProxyURL
+    }
+  );
+}
+
+var httpsOpts = {
+  key: fs.readFileSync(__dirname + '/fixtures/test.key'),
+  cert: fs.readFileSync(__dirname + '/fixtures/test.crt')
+};
+
+var server = https.createServer(httpsOpts, app);
+server.listen(3000);
+
+console.log('Listening on port 3000.');
