c34dd1b8b1e2ffc69c34a318b6833c5cc40c973b
Jim Kang <jimkang@gmail.com>
2015-07-23 22:48:53 -0400

    - Fixed routes and proxies in server. - Added example episode and me json files for server to serve.

--- server.js
+++ server.js
@@ -5,57 +5,71 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 
 var matterhornProxyURL = 'https://matterhorn.dce.harvard.edu/';
-var mostRecentWatchReqUrl;
-var cannedEpisode = jsonfile.readFileSync(__dirname + '/fixtures/live.json');
+var proxyOpts = {
+  target: matterhornProxyURL
+};
 
-var app = express();
+var mostRecentWatchReqUrl;
+
+var cannedEpisode = jsonfile.readFileSync(
+  __dirname + '/fixtures/example-episode.json'
+);
+
+var cannedMe = jsonfile.readFileSync(
+  __dirname + '/fixtures/example-me.json'
+);
 
 var proxy = httpProxy.createProxyServer({
   secure: false
 });
 
-// Serve /engage/player/* requests from the local build folder.
-app.use('/engage/player', express.static('build'));
+var app = express();
 
-// Handle login.html requests with a redirect.
-app.get('/login.html', skipToContent);
-app.get('/info/me.json', skipToContent);
+var router = express.Router();
 
-// Serve a canned episode for episode requests.
-// app.get('/search/episode.json', episode);
+// // Handle login.html requests with a redirect.
+router.get('/login.html*', skipToContent);
+router.get('/info/me.json*', me);
+
+// // Serve a canned episode for episode requests.
+router.get('/search/episode.json*', episode);
 
 // Handle everything else with the proxy back to the Matterhorn server.
-app.use(passToProxy);
+router.get('/*', passToProxy);
+
+
+// Serve /engage/player/* requests from the local build folder.
+app.use('/engage/player', express.static('build'));
+app.use('/engage/player', express.static('build/resources'));
+app.use('/', router);
 
 
 function skipToContent(req, res, next) {
+  console.log('Skipping to', mostRecentWatchReqUrl);
   if (mostRecentWatchReqUrl) {
     res.redirect(mostRecentWatchReqUrl);
   }
-  else {
-    next();
-  }
+  next();
 }
 
-function episode(req, res, next) {
+function episode(req, res) {
   console.log('Serving episode.');
   res.json(cannedEpisode);
 }
 
-function passToProxy(req, res, next) {
+function me(req, res) {
+  console.log('Serving me.json.');
+  res.json(cannedMe);
+}
+
+function passToProxy(req, res) {
   console.log('Proxying:', req.url);
 
   if (req.url.indexOf('/player/watch.html') === 0) {
     mostRecentWatchReqUrl = req.url;
   }
 
-  proxy.web(
-    req,
-    res,
-    {
-      target: matterhornProxyURL
-    }
-  );
+  proxy.web(req, res, proxyOpts);
 }
 
 var httpsOpts = {
